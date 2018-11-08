4931f0b18c1f35958469bcbc001350024817a5e8
Andrés Osante <aosantea@ucenfote.ac.cr>
2018-11-07 23:24:19 -0600

    final changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCegJVh5SmaUnFDfx5JPhPF2qfD3WB6CkI',
+    authDomain: 'fs-task-manager.firebaseapp.com',
+    databaseURL: 'https://fs-task-manager.firebaseio.com',
+    projectId: 'fs-task-manager',
+    storageBucket: 'fs-task-manager.appspot.com',
+    messagingSenderId: '35097990551'
+  }
 };
